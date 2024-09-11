36b49b69432b53d24a0f8ccc49e98b82fc4a53c1
NathanielS9872 <nathanielspr@gmail.com>
2024-09-10 19:43:39 -0500

    Homepage and Dynamic Welcome

--- site.js
+++ site.js
@@ -1,1 +1,16 @@
 console.log("Test")
+const div = document.querySelector('#welcome')
+const hours = new Date().getHours()
+const isMorning = hours >= 4 && hours < 12
+const isAfternoon = hours >= 12 && hours < 17
+const isEvening = hours >= 17 || hours < 4
+
+if (isMorning){
+    div.textContent = "Good Morning"
+}
+if(isAfternoon){
+    div.textContent = "Good Afternoon"
+}
+if(isEvening){
+    div.textContent = "Good Evening"
+}
