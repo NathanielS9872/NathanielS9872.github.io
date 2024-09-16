f650e3a79bd135526000737b914cb81a657f7936
NathanielS9872 <nathanielspr@gmail.com>
2024-09-16 14:03:20 -0500

    Added Secret Message

--- site.js
+++ site.js
@@ -4,7 +4,6 @@ const hours = new Date().getHours()
 const isMorning = hours >= 4 && hours < 12
 const isAfternoon = hours >= 12 && hours < 17
 const isEvening = hours >= 17 || hours < 4
-
 if (isMorning){
     div.textContent = "Good Morning"
 }
@@ -14,3 +13,5 @@ if(isAfternoon){
 if(isEvening){
     div.textContent = "Good Evening"
 }
+
+localStorage.setItem("It's a secret to everybody.","It's not secure to everybody.")
