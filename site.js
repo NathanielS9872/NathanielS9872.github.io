005484e0da39bc89e39e4e9545642c60ecaf7d21
NathanielS9872 <nathanielspr@gmail.com>
2024-10-21 12:27:39 -0500

    pokeAPI
    also added if statements to check the title of the document(s) to separate the code

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
-//Homepage
+//All pages
 //console.log("Test")
 const div = document.querySelector('#welcome')
 const hours = new Date().getHours()
@@ -14,57 +14,84 @@ if(isAfternoon){
 if(isEvening){
     div.textContent = "Good Evening"
 }
-
 //localStorage.setItem("It's a secret to everybody.","It's not secure to everybody.")
 
-//Image Carousel
-const urls = [
-    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-].map(url => { (new Image()).src = url; return url })
+//Image Carousel 
+if(document.title == "Image Carousel"){
+    const urls = [
+        'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    ].map(url => { (new Image()).src = url; return url })
 
-const images = document.querySelectorAll('#carousel img')
+    const images = document.querySelectorAll('#carousel img')
 
-let currentImage = 0
-const showImages = () => {
-    const offset = currentImage % urls.length
-    images.forEach((image, index) => {
-        const imageIndex = (index + offset + urls.length) % urls.length
-        image.src = urls[imageIndex]
+    let currentImage = 0
+    const showImages = () => {
+        const offset = currentImage % urls.length
+        images.forEach((image, index) => {
+            const imageIndex = (index + offset + urls.length) % urls.length
+            image.src = urls[imageIndex]
+        })
+    }
+    showImages()
+    setInterval(()=>{
+        currentImage++
+        showImages();
+    },5000)
+    document.querySelector("#prev").addEventListener("click",()=>{
+        currentImage--
+        showImages()
+    })
+    document.querySelector("#next").addEventListener("click",()=>{
+        currentImage++
+        showImages()
     })
 }
-showImages()
-setInterval(()=>{
-    currentImage++
-    showImages();
-},5000)
-document.querySelector("#prev").addEventListener("click",()=>{
-    currentImage--
-    showImages()
-})
-document.querySelector("#next").addEventListener("click",()=>{
-    currentImage++
-    showImages()
-})
 
-//todo-list
-const todoinput = document.querySelector("#new-todo")
-const todoList = document.querySelector(".todo-list")
-renderTodos = () =>{
-    todoList.innerHTML = ' '
-    JSON.parse(localStorage.getItem('todo-list')).forEach(({text})=>{
-        const li = document.createElement('li')
-        li.textContent = text
-        todoList.append(li)
-    })}
-let todos = localStorage.getItem('todo-list')||[]
-if(todos == localStorage.getItem('todo-list'))renderTodos()
-document.querySelector("#toDoButton").addEventListener("click",()=>{
-    let todos = JSON.parse(localStorage.getItem('todo-list'))||[]
-    todos.push({text:todoinput.value, completed:false})
-    localStorage.setItem('todo-list',JSON.stringify(todos))
-    renderTodos()
-})
+//To-Do List
+if (document.title == "To-Do List"){
+    const todoinput = document.querySelector("#new-todo")
+    const todoList = document.querySelector(".todo-list")
+    renderTodos = () =>{
+        todoList.innerHTML = ' '
+        JSON.parse(localStorage.getItem('todo-list')).forEach(({text})=>{
+            const li = document.createElement('li')
+            li.textContent = text
+            todoList.append(li)
+        })}
+    let todos = localStorage.getItem('todo-list')||[]
+    if(todos == localStorage.getItem('todo-list'))renderTodos()
+    document.querySelector("#toDoButton").addEventListener("click",()=>{
+        let todos = JSON.parse(localStorage.getItem('todo-list'))||[]
+        todos.push({text:todoinput.value, completed:false})
+        localStorage.setItem('todo-list',JSON.stringify(todos))
+        renderTodos()
+    })
+}
+
+//Homepage (API-Fetch)
+if (document.title == "Homepage"){
+    const img = document.createElement("img")
+    const div = document.querySelector("#pokeAPI")
+    const getRandomPokemon = async () =>{
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/"+Math.floor(Math.random()*150))
+        const data = await response.json()
+        return data
+    }
+    const renderPokemon = pokemon =>{
+        img.src = pokemon.sprites.front_default
+        img.alt = pokemon.name
+        div.append(img)
+    }
+    (async()=>{
+        try{
+            const randomPokemon = await getRandomPokemon()
+            renderPokemon(randomPokemon)
+        }catch{
+            console.log("Error getting and loading pokemon")
+        }
+    })()
+}
